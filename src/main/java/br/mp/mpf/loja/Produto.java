1d545b118bf9ee51b3347c47d10c09b5a92e71e5
andrethiago <andrethiago@devel>
2017-06-19 22:49:50 -0300

    Adicionando o construtor à classe Produto.

--- src/main/java/br/mp/mpf/loja/Produto.java
+++ src/main/java/br/mp/mpf/loja/Produto.java
@@ -2,8 +2,19 @@ package br.mp.mpf.loja;
 
 public class Produto {
 
+	private String nome;
+
 	private Double valor;
-	
+
+	public Produto(String nome, Double valor) {
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
 	public Double getValor() {
 		return valor;
 	}
